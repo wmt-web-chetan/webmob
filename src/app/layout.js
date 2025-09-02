cc54b1f69058d506ee13399e4ab817ab190d07b4
Sagar Dhoriya <sagard@webmobtech.com>
2025-09-02 14:55:01 +0530

    adding footer banner

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/Footer";
 
 // Load Inter font
 const inter = Inter({
@@ -19,6 +20,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${inter.variable} antialiased`}>
         {children}
+        <Footer />
       </body>
     </html>
   );
