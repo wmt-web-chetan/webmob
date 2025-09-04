ab175dc3a3e5c7c59e397d511f7720be52265e2a
Sagar Dhoriya <sagard@webmobtech.com>
2025-09-04 15:34:09 +0530

    working case study detail page

--- src/sections/CaseStudyDetailPage/HeroSection/index.js
+++ src/sections/CaseStudyDetailPage/HeroSection/index.js
@@ -1,44 +1,31 @@
-"use client"
-import React from "react"
+import React from "react";
+import Image from "next/image";
 
 export const CaseHeroSection = () => {
   return (
     <>
-      <div
-        className="w-full bg-cover bg-center h-screen overflow-hidden relative"
-        style={{
-          backgroundImage: `url('https://webmobtechcdn.nyc3.cdn.digitaloceanspaces.com/wmt_v4/2023/04/Career.webp')`, // Replace with your actual image URL
-        }}
-      >
-        {/* Content */}
-        <div 
-          className="relative flex items-center justify-center h-full w-full"
-          style={{ backgroundColor: 'rgba(0, 0, 0, 0.7)' }}
-        >
-          <div
-            className="flex flex-col place-items-center items-center text-center py-16 m-4 max-w-5xl"
-            data-aos="fade-up"
-            data-aos-easing="ease-out-back"
-            data-aos-duration="700"
-          >
-            {/* Main Heading */}
-            <h1 className="font-serif font-bold lg:text-5xl text-4xl text-white max-w-7xl h1-heading" style={{ lineHeight: "1.20" }}>
-                Viwa Wireless
-                </h1>
-            <hr
-                  className="h-1 w-40 mt-5 border-0 bg-white"
-                />
-            {/* Subtitle */}
-            <p className="text-white pt-6 text-lg font-sans max-w-4xl mx-auto">
-            Robust B2B/B2C eCommerce and POS Solution with Dynamic Pricing, Marketplace Integration, and Real-Time Inventory Sync
+      <div className="w-full h-screen overflow-hidden relative">
+        <div className="w-full h-full flex flex-col  items-center justify-between gap-y-8 sm:gap-y-10 xl:gap-y-14 pt-24 sm:pt-32 lg:pt-40">
+          <div className="max-w-4xl flex flex-col gap-4 px-4 sm:px-6 lg:px-8">
+            <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold text-center leading-snug">
+              Viva Wireless
+            </h1>
+            <p className="text-base sm:text-lg lg:text-2xl font-normal text-center leading-relaxed">
+              Robust B2B/B2C eCommerce and POS Solution with Dynamic Pricing,
+              Marketplace Integration, and Real-Time Inventory Sync
             </p>
-            
-        
           </div>
+          <Image
+            src="https://webmobtechcdn.nyc3.cdn.digitaloceanspaces.com/wmt_v4/2025/08/0d6a55646e9aa800ca19a86402554b868c16be30.png"
+            alt="hero-img"
+            width={1800}
+            height={1800}
+            className="w-full h-3/4  xl:w-full xl:h-auto object-cover "
+          />
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default CaseHeroSection
+export default CaseHeroSection;
